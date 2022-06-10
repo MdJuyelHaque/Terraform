b7f68c1aad5420c6010005d22429220b467dbcb9
Md Juyel Haque <78420794+MdJuyelHaque@users.noreply.github.com>
2022-06-10 16:19:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,8 @@ terraform {
   }
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
 
-provider "local" {
-  version = "~> 1.2"
-}
+
 
 resource "aws_instance" "myinstance" {
   instance_type ="t2.micro"
