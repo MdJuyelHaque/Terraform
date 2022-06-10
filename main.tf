ca77a5117aa15e02e03fc7f7ebba21fd369dc2ab
Md Juyel Haque <78420794+MdJuyelHaque@users.noreply.github.com>
2022-06-10 15:08:34 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "1.1.9"
+  
+  backend "s3" {
+    bucket = "bti-terraform-aws-state"
+    key = "ec2-deployment/terraform.tfstate"
+    region = "us-west-2"
+    
+  }
+}
+
+provider "aws" {
+  shared_credentials_file = "C:\\Users\\user\\.aws\\credentials"
+  profile                 = "default"
+  region                  = "us-west-2"
+}
+
+resource "aws_instance" "myinstance" {
+  instance_type ="t2.micro"
+  ami ="ami-0ed984ee7fb7202ea"
+  key_name ="test"
+  
+}
